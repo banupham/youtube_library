2cc18578ef517f4d40b551a12d67d9dcc0e5aa73
banupham <135731801+banupham@users.noreply.github.com>
2026-08-23 21:57:34 +0700

    Add Android collector app build config

---
+++ android_collector/app/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.youtube.library.collector"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.youtube.library.collector"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
